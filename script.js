54225c0d012a0a137d9d938e6bed834459f3bb71
Andrew Kyle Gopez <akgopez@gmail.com>
2026-05-16 18:21:32 -0400

    Remove script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function beginJourney() {
-    const landing = document.querySelector(".landing-page");
-    const main = document.querySelector(".main-content");
-
-    if (landing && main) {
-        landing.classList.add("hidden");
-        main.classList.remove("hidden");
-    }
-}
